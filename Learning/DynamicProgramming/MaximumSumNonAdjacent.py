9fd2a72449ffa8c6d09b184ece17127e6c394293
MAC <mac@MACdeMacBook-Pro.local>
2025-10-12 20:18:53 -0300

    :(

--- Learning/DynamicProgramming/MaximumSumNonAdjacent.py
+++ Learning/DynamicProgramming/MaximumSumNonAdjacent.py
@@ -17,7 +17,7 @@ def max_sub_sequences(s, i=0, r=[]):
 # Now the problem states to find all subsequences with no adjacents elements
 # This makes more sence, because you cannot take a greedy approach and need to test
 # every subsequence
-# Time complexity: 0(2^n)
+# Time complexity: O(2^n)
 # Space complexity: O(n)
 def no_adj_max_sub_sequences(s, i=0, r=[]):
     if i >= len(s):
@@ -31,27 +31,54 @@ def no_adj_max_sub_sequences(s, i=0, r=[]):
     not_taken = no_adj_max_sub_sequences(s, i+1, r)
     return max(taken, not_taken)
 
-# Optimization using dp
-def no_adj_max_sub_seq(s, i=0, d={})
+# Optimization using memoization
+# Space complexity: O(n)
+# Time complexity: O(n)
+def no_adj_max_sub_seq(s, i=0, memo={})
     if i >= len(s):
         return 0
 
-    if i in d:
-        return d[i]
+    if i in memo:
+        return memo[i]
     
-    r.append(s[i])
-    if i+2 in d:
-        taken = d[i+2]
-    else:
-        taken = s[i] + no_adj_max_sub_seq(s, i+2, d)
-    
-    r.pop()
-    if i+1 in d:
-        not_taken = d[i+1]
-    else:
-        not_taken = no_adj_max_sub_seq(s, i+1, d)
-    d[i] = max(taken, not_taken)
-    return d[i]
+    taken = s[i] + no_adj_max_sub_seq(s, i+2, d)
+    not_taken = no_adj_max_sub_seq(s, i+1, d)
+    memo[i] = max(taken, not_taken)
+    return memo[i]
 
+# Optimization using tabulation
+# Time complexity: O(n)
+# Space complexity: O(n)
+def no_adj_max_sub_seq_tab(s):
+    n = len(s):
+    dp = [0] * n
+    dp[0] = s[0]
+    dp[1] = max(s[0], s[1])
 
-print(no_adj_max_sub_sequences(s))
+    for i in range(2, n):
+        taken = s[i] + dp[i-2]
+        not_taken = dp[i-1]
+        dp[i] = max(taken, not_taken)
+    return dp[n-1]
+
+# Optimization using current and previus 
+# Time complexity: O(n)
+# Space complexity: O(1)
+def no_adj_max_sub_seq_pp(s):
+    n = len(s)
+    # As i am going from 2 to n, i need to handle te edge cases when n = 1 or n = 0
+    if n == 0:
+        return 0
+    elif n == 1:
+        return s[0]
+
+    previous = s[0]
+    current = max(s[0], s[1])
+
+    for i in range(2, n):
+        taken = s[i] + previous
+        n = max(taken, current)
+        previous = current
+        current = n
+
+    return current
