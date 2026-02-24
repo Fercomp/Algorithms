f6d4fc01221819cdec056cdb3106c312ded71ce1
MAC <mac@MACdeMacBook-Pro.local>
2026-02-24 00:00:40 -0300

    :(

--- Learning/BinarySearch/BinarySearch.py
+++ Learning/BinarySearch/BinarySearch.py
@@ -1,17 +1,12 @@
 nums = [3, 5, 7, 8, 10, 12]
 target = 7
 
-"""
-Why do we use <= instead of < in binary search?
+# Why do we use <= instead of < in binary search?
+# We must keep searching even when left and right point to the same index.
+# If we used <, the loop would stop before checking the last possible element, which could be the target.
+# Using <= guarantees that we still test the case where left == right.
+# OBS: Is middle not midlle
 
-We must keep searching even when left and right point to the same index.
-If we used <, the loop would stop before checking the last possible element,
-which could be the target.
-
-Using <= guarantees that we still test the case where left == right.
-
-OBS: Is middle not midlle
-"""
 def binary_search(nums, target):
     left_ptr = 0
     right_ptr = len(nums) - 1
